38497ffbe776a3d3aa990a326a6d45be4c7e2d01
郭亮 <2292735470@qq.com>
2019-07-09 17:43:36 +0800

    reset

--- www/shell-exec.js
+++ www/shell-exec.js
@@ -3,7 +3,7 @@ var exec = require("cordova/exec");
 exports.exec = function (cmd, callback) {
   exec(callback, function (err) {
     callback({exitStatus: 100, output: err});
-  }, "cordova-plugin-shell-exec", "exec", [cmd]);
+  }, "ShellExec", "exec", [cmd]);
 };
 
 exports.setTime = function (time, success, error) {
