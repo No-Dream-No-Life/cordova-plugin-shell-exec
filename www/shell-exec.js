58d08af68e5f0ef6aa6c98fee84a47d1d5607c66
郭亮 <2292735470@qq.com>
2019-07-09 18:38:06 +0800

    reset

--- www/shell-exec.js
+++ www/shell-exec.js
@@ -1,6 +1,6 @@
 var exec = require("cordova/exec");
 
-exports.exec = function (cmd, callback) {
+exports.rootExec = function (cmd, callback) {
   exec(callback, function (err) {
     callback({exitStatus: 100, output: err});
   }, "ShellExec", "rootExec", cmd);
